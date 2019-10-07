8c5a6c618f5567ee92167b3cd0f524b3ef3269da
xrachitx <rachit18302@iiitd.ac.in>
2019-10-08 01:09:29 +0530

    third present for u

--- Assmbr.py
+++ Assmbr.py
@@ -1,35 +1,35 @@
-# def addToOpcode(instruction,address,assemblyToOpcode):
-#     opcodeTableValue = ["null","null","null","null","null"]
-#     if (instruction[0] == "STP" or instruction[0] == "CLA"):
-#         opcodeTableValue[0] = instruction[0]
-#         opcodeTableValue[4] = assemblyToOpcode[instruction[0]]
-#         return opcodeTableValue
-#     elif (instruction[0] == "LAC" or instruction[0] == "SAC" or instruction[0] == "ADD" or instruction[0] == "SUB" or instruction[0] =="INP" or instruction[0] =="DSP" or instruction[0] =="MUL" or instruction[0] =="BRZ" or instruction[0] =="BRP" or instruction[0] =="BRN"):
-#         opcodeTableValue[0] = instruction[0]
-#         opcodeTableValue[4] = assemblyToOpcode[instruction[0]]
-#         if (instruction[0] =="BRZ" or instruction[0] =="BRP" or instruction[0] =="BRN"):
-#             opcodeTableValue[1] = instruction[1]
-#             return opcodeTableValue
-#         else:
+def addToOpcode(instruction,address,assemblyToOpcode):
+    opcodeTableValue = ["null","null","null","null","null"]
+    if (instruction[0] == "STP" or instruction[0] == "CLA"):
+        opcodeTableValue[0] = instruction[0]
+        opcodeTableValue[4] = assemblyToOpcode[instruction[0]]
+        return opcodeTableValue
+    elif (instruction[0] == "LAC" or instruction[0] == "SAC" or instruction[0] == "ADD" or instruction[0] == "SUB" or instruction[0] =="INP" or instruction[0] =="DSP" or instruction[0] =="MUL" or instruction[0] =="BRZ" or instruction[0] =="BRP" or instruction[0] =="BRN"):
+        opcodeTableValue[0] = instruction[0]
+        opcodeTableValue[4] = assemblyToOpcode[instruction[0]]
+        if (instruction[0] =="BRZ" or instruction[0] =="BRP" or instruction[0] =="BRN"):
+            opcodeTableValue[1] = instruction[1]
+            return opcodeTableValue
+        else:
 
 
 
 
 
-# def firstPass(inpt,memory):
-#     symbolTable = {}
-#     opcodeTable= {}
-#     literalTable = {}
-#     unattendedLabels = {}
-#     for i in range(len(inpt)):
-#         instruction = inpt[i]
-#         if (len(instruction)>=3):
-#             if (instruction[0]!="DIV"):
-#                 symbolTable[instruction[0]]=i
-#                 # if instruction[1] == "STP" or instruction[1] == "CLA":
-#                 #     opcodeTable[i] = ("STP")
-#                 #     pass
-#                 # elif symbolTable
+def firstPass(inpt,memory):
+    symbolTable = {}
+    opcodeTable= {}
+    literalTable = {}
+    unattendedLabels = {}
+    for i in range(len(inpt)):
+        instruction = inpt[i]
+        if (len(instruction)>=3):
+            if (instruction[0]!="DIV"):
+                symbolTable[instruction[0]]=i
+                # if instruction[1] == "STP" or instruction[1] == "CLA":
+                #     opcodeTable[i] = ("STP")
+                #     pass
+                # elif symbolTable
 
 
 
@@ -42,8 +42,6 @@ if __name__ == "__main__":
             memory[i] = 32767-i
         else:
             memory[i] = "nullValue"
-    print(memory[28671])
-    print(memory[32767])
     # print(assemblyToOpcode)
     print("Make sure that the input file is in the same directory as the Assemblr.py file")
     print("Enter the name for the input file.")
