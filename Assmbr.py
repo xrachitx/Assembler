d8ffdaae4da9648d93ed343411fad0d3f8a7d134
sidddddd-18 <siddharth.sadhwani@gmail.com>
2019-10-08 14:59:13 +0530

    wassup jigglipuff

--- Assmbr.py
+++ Assmbr.py
@@ -26,6 +26,10 @@ def addToOpcode(instruction,address,assemblyToOpcode):
         if (instruction[0] =="BRZ" or instruction[0] =="BRP" or instruction[0] =="BRN"):
             opcodeTableValue[1] = instruction[1]
             return opcodeTableValue
+        elif (instruction[1][0]=="'" or instruction[1][0]=='"'):
+            const=int(instruction[1][1:-1])
+
+
         else:
             if (instruction[1].isdigit()):
                 if int(instruction[1]) <= 28671:
@@ -34,6 +38,7 @@ def addToOpcode(instruction,address,assemblyToOpcode):
                     print("Memory address "+ instruction[1]+" not available. Memory address cannot exceed 28671")
             else:
                 print("Instruction format error: "+ instruction[1]+" is not a valid memory address.")
+
     elif (instruction[0]=="DIV"):
         if (len(instruction)<4):
             joined_string = ' '.join([str(v) for v in instruction])
@@ -56,44 +61,3 @@ def addToOpcode(instruction,address,assemblyToOpcode):
 
     else:
         print("Not a valid instruction.")
-
-
-
-
-
-def firstPass(inpt,memory):
-    symbolTable = {}
-    opcodeTable= {}
-    literalTable = {}
-    unattendedLabels = {}
-    for i in range(len(inpt)):
-        instruction = inpt[i]
-        if (len(instruction)>=3):
-            if (instruction[0]!="DIV"):
-                symbolTable[instruction[0]]=i
-                # if instruction[1] == "STP" or instruction[1] == "CLA":
-                #     opcodeTable[i] = ("STP")
-                #     pass
-                # elif symbolTable
-
-
-
-if __name__ == "__main__":
-    assemblyToOpcode = {"CLA":"0000","LAC":"0001","SAC": "0010","ADD":"0011","SUB":"0100", "BRZ":"0101","BRN":"0110","BRP":"0111","INP":"1000","DSP":"1001","MUL":"1010","DIV":"1011","STP":"1100"}
-    memory = {}
-    for i in range(32768):
-        # print(int(str(bin(i+1))[2:]))
-        if (i>28671):
-            memory[i] = 32767-i
-        else:
-            memory[i] = "nullValue"
-    # print(assemblyToOpcode)
-    print("Make sure that the input file is in the same directory as the Assemblr.py file")
-    print("Enter the name for the input file.")
-    name = input()     
-    f = open(name, "r")
-    l = []
-    for x in f:
-        l.append(x.split())
-    print(l)
-    # for i in l:
