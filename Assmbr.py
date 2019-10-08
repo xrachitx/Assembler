dbedb2f526205babff8d718fa720fa1d949ad13b
xrachitx <rachit18302@iiitd.ac.in>
2019-10-08 15:45:07 +0530

    reverted the repo to normal

--- Assmbr.py
+++ Assmbr.py
@@ -1,26 +1,10 @@
 def addToOpcode(instruction,address,assemblyToOpcode):
     opcodeTableValue = ["null","null","null","null","null"]
     if (instruction[0] == "STP" or instruction[0] == "CLA"):
-        try:
-            x = instruction[1]
-            joined_string = ' '.join([str(v) for v in instruction])
-            print("Error: Too many arguments in the instruction- "+ joined_string)
-        except IndexError:
-            pass
         opcodeTableValue[0] = instruction[0]
         opcodeTableValue[4] = assemblyToOpcode[instruction[0]]
         return opcodeTableValue
     elif (instruction[0] == "LAC" or instruction[0] == "SAC" or instruction[0] == "ADD" or instruction[0] == "SUB" or instruction[0] =="INP" or instruction[0] =="DSP" or instruction[0] =="MUL" or instruction[0] =="BRZ" or instruction[0] =="BRP" or instruction[0] =="BRN"):
-        if (len(instruction)<2):
-            joined_string = ' '.join([str(v) for v in instruction])
-            print("Error: Less arguments in the instruction- "+joined_string)
-        try:
-            x = instruction[2]
-            joined_string = ' '.join([str(v) for v in instruction])
-            print("Error: Too many arguments in the instruction- "+ joined_string)
-        except IndexError:
-            pass
-
         opcodeTableValue[0] = instruction[0]
         opcodeTableValue[4] = assemblyToOpcode[instruction[0]]
         if (instruction[0] =="BRZ" or instruction[0] =="BRP" or instruction[0] =="BRN"):
@@ -61,3 +45,44 @@ def addToOpcode(instruction,address,assemblyToOpcode):
 
     else:
         print("Not a valid instruction.")
+
+
+
+
+
+def firstPass(inpt,memory):
+    symbolTable = {}
+    opcodeTable= {}
+    literalTable = {}
+    unattendedLabels = {}
+    for i in range(len(inpt)):
+        instruction = inpt[i]
+        if (len(instruction)>=3):
+            if (instruction[0]!="DIV"):
+                symbolTable[instruction[0]]=i
+                # if instruction[1] == "STP" or instruction[1] == "CLA":
+                #     opcodeTable[i] = ("STP")
+                #     pass
+                # elif symbolTable
+
+
+
+if __name__ == "__main__":
+    assemblyToOpcode = {"CLA":"0000","LAC":"0001","SAC": "0010","ADD":"0011","SUB":"0100", "BRZ":"0101","BRN":"0110","BRP":"0111","INP":"1000","DSP":"1001","MUL":"1010","DIV":"1011","STP":"1100"}
+    memory = {}
+    for i in range(32768):
+        # print(int(str(bin(i+1))[2:]))
+        if (i>28671):
+            memory[i] = 32767-i
+        else:
+            memory[i] = "nullValue"
+    # print(assemblyToOpcode)
+    print("Make sure that the input file is in the same directory as the Assemblr.py file")
+    print("Enter the name for the input file.")
+    name = input()     
+    f = open(name, "r")
+    l = []
+    for x in f:
+        l.append(x.split())
+    print(l)
+    # for i in l:
