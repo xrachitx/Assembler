284f439834f838506d8f876fa5665d5fe89c43b5
xrachitx <rachit18302@iiitd.ac.in>
2019-10-08 15:49:01 +0530

    memLocUsed set created

--- Assmbr.py
+++ Assmbr.py
@@ -70,10 +70,12 @@ def firstPass(inpt,memory):
 if __name__ == "__main__":
     assemblyToOpcode = {"CLA":"0000","LAC":"0001","SAC": "0010","ADD":"0011","SUB":"0100", "BRZ":"0101","BRN":"0110","BRP":"0111","INP":"1000","DSP":"1001","MUL":"1010","DIV":"1011","STP":"1100"}
     memory = {}
+    memLocsUsed = set()
     for i in range(32768):
         # print(int(str(bin(i+1))[2:]))
         if (i>28671):
             memory[i] = 32767-i
+            memLocsUsed.add(i)
         else:
             memory[i] = "nullValue"
     # print(assemblyToOpcode)
